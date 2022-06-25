beff65d1812522e8d461ee68113224e3643f8635
Ayberk Özkan <kenan.ayberk@hotmail.com>
2022-06-25 15:22:11 +0300

    Old files deleted

--- 2-Math Operators/Membership_Operators.py
+++
@@ -1,8 +0,0 @@
-# Karakter dizisi veya bir sayı, bir veri tipinin içerisinde bulunuyorsa bunu kontrol etmemizi sağlarlar.
-# in --> Belirtilen değere sahip bir dizi varsa True
-# not in --> Belirtilen değere sahip bir dizi yoksa True
-# ! ! ! Sayısal değerlerde kullanılmaz. ! ! !
-
-isim = "Ayberk"
-print("A" in isim)
-print("a" in isim)
---
+++ test.py
@@ -0,0 +1,4 @@
+#Unpacking
+Meyveler = ["Armut", "Muz", "Portakal"]
+s, d, f = Meyveler
+print(s, d, f)
